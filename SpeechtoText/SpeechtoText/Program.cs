4a924e9ffb5e93e84e352e8965936deb412b66e1
Laura Martinez Klein <lmartinezklein@gmail.com>
2021-05-24 12:20:53 -0400

    Estructurar funciones y agregar paquetes Nuget

---
+++ SpeechtoText/SpeechtoText/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Threading.Tasks;
+
+namespace SpeechtoText
+{
+    class Program
+    {
+        public static async Task RecognizeSpeechAsync()
+        {
+            var config = SpeechConfig.FromSubscription("a1befbc4850b475eb03bd4c706b2766b", "eastus");
+
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something...");
+
+
+                var result = await recognizer.RecognizeOnceAsync();
+
+            
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
+        }
+
+        static async Task Main()
+        {
+            await RecognizeSpeechAsync();
+            Console.WriteLine("Please press <Return> to continue.");
+            Console.ReadLine();
+        }
+    }
+}
+
